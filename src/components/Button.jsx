43963f554250724adcef35b84f1c28708811e205
Shivam Sharma <meshivam81@gmail.com>
2023-08-23 22:24:27 +0530

    hero section and shoe cards

---
+++ src/components/Button.jsx
@@ -0,0 +1,19 @@
+ 
+const Button = ({lable, iconUrl}) => {
+  return (
+     <button className="flex
+      justify-center item-center gap-2
+      px-7 py-4 border leading-none font-montserrat
+      text-lg bg-coral-red text-white rounded-full">
+        {lable}
+
+        <img
+         src={iconUrl}
+         alt="icon"
+         className="ml-2 rounded-full w-5 h-5"
+         />
+     </button>
+  )
+}
+
+export default Button
