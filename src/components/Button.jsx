8653cda479920c07a39b712f4e66530b5abbe03a
Shivam Sharma <meshivam81@gmail.com>
2023-08-25 09:18:41 +0530

    sign up and footer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,12 @@
  
-const Button = ({lable, iconUrl, backgroundColor, borderColor, textColor}) => {
+const Button = ({lable, iconUrl, backgroundColor, borderColor, textColor, fullWidth}) => {
   return (
      <button className={`rounded-full flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
      ${
        backgroundColor
          ? `${backgroundColor} ${textColor} ${borderColor}`
-         : "bg-coral-red text-white border-coral-red"}`}
+         : "bg-coral-red text-white border-coral-red"} rounded-full ${fullWidth &&
+       'w-full'}'}`}
          >
         {lable}
 
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,9 +1,62 @@
+import { copyrightSign } from "../assets/icons"
+import { footerLogo } from "../assets/images"
+import { footerLinks, socialMedia } from "../constants"
+
  
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+     <footer className="max-container">
+      <div className="flex justify-between items-start
+       gap-20 flex-wrap max-lg:flex-col">
+       <div className=" flex flex-col items-start">
+        <a href="/">
+          <img 
+          src={footerLogo}
+          alt="nike"
+          width={130}
+          height={40} />
+        </a>
+        <p className="mt-4 sm:max-w-sm">Get shoes ready to your nearest Nike store. Find your perfect size</p>
+         <div className="flex justify-center items-center gap-5 mt-8">
+          {socialMedia.map((social) => (
+            <div key={social} className="flex justify-center items-center w-11 h-11 bg bg-white rounded-full cursor-pointer">
+              <img 
+              src={social.src} 
+              alt={social.alt}
+              width={27}
+              height={24}/>
+            </div>
+          ))}
+         </div>
+       </div>
+       <div className="flex flex-1 justify-between flex-wrap gap-20 lg:gap-10 ">
+        {footerLinks.map((section)=> (
+          <div key={section}>
+           <h4 className="text-2xl font-montserrat">{section.title}</h4>
+           <ul>
+            {section.links.map((link) => (
+              <li key={link} className="mt-3 hover:text-slate-gray cursor-pointer">
+                <a>{link.name}</a>
+              </li>
+            ))}
+           </ul>
+          </div>
+        ))}
+       </div>
+      </div>
+       <div className="flex justify-between mt-20 max-sm:flex-col max-sm:items-center">
+        <div className="flex flex-1 justify-start items-center font-montserrat cursor-pointer">
+         <img 
+         src={copyrightSign} 
+         alt="copy right sign" 
+         width={20} 
+         height={20} 
+         className="rounded-full m-0" />
+         <p>Copyright. All rigths reserved</p>
+        </div>
+        <p>Terms & Conditions</p>
+       </div>
+     </footer>
   )
 }
 
