208fa48ca2ae60bc358904449cb4bf7302dbcf79
Vayras <89837102+Vayras@users.noreply.github.com>
2023-08-28 18:40:23 +0530

    ui changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { headerLogo } from "../assets/images";
 import { navLinks } from "../constants";
 const Navbar = () => {
   return (
-    <header className="padding-x py-8 absolute z-10 w-full">
+    <header className="absolute z-10 py-8 w-full padding-x">
       <nav className="flex justify-between items-center max-container">
         <a href="/">
           <img 
@@ -12,12 +12,12 @@ const Navbar = () => {
           width={125}
           height={29} />
         </a>
-        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+        <ul className="flex flex-1 gap-16 justify-end items-center mr-32 max-lg:hidden">
           {navLinks.map((item) => (
             <li key={item.label}>
               <a
                 href={item.href}
-                className="font-montserrat leading-normal hover:underline text-lg text-slate-gray"
+                className="text-lg font-semibold leading-normal font-montserrat hover:underline text-coral-red"
               >
                 {item.label}
               </a>
