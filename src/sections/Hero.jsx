3ca8ad5a63f79e8dab473db818522ec6fef900c2
Vayras <89837102+Vayras@users.noreply.github.com>
2023-08-28 18:40:23 +0530

    ui changes

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -12,45 +12,41 @@ const Hero = () => {
   return (
     <section
       id="home"
-      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container">
-      <div className="relative xl:w-2/5 flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+      className="flex flex-col gap-32 justify-center w-full min-h-screen xl:flex-row max-container">
+      <div className="relative flex-col justify-center items-start pt-28 w-full xl:w-2/5 max-xl:padding-x">
         <p className="text-xl font-montserrat text-coral-red">Our Collection</p>
         <h1 className="mt-10 font-palanquin text-8xl
          max-sm:text-[72] max-sm:leading-[82] font-bold">
-          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New arrival</span>
+          <span className="relative z-10 pr-10 xl:whitespace-nowrap">The New arrival</span>
           <br />
-          <span className="text-coral-red inline-block mt-3">Nike</span>Shoes
+          <span className="inline-block mt-3 text-coral-red">Nike</span>Shoes
         </h1>
-        <p className="font-montserrat text-slate-gray mt-8 mb-10
-         text-lg leading-7 sm:max-w-sm">
+        <p className="mt-8 mb-10 text-lg leading-7 font-montserrat text-slate-gray sm:max-w-sm">
           Discover new stylish Nike arrivals, comfort, best quality and
           inovation for your active life.
         </p>
         <Button lable="Shop now" iconUrl={arrowRight} />
 
-        <div className="flex
-         justify-start items-start flex-wrap w-full mt-20
-          gap-16">
+        <div className="flex flex-wrap gap-16 justify-start items-start mt-20 w-full">
           {statistics.map((statistic) => (
             <div key={statistic.label}>
-              <p className="text-4xl font-palanquin font-bold">{statistic.value}</p>
-              <p className="leading-1 text-slate-gray font-palanquin font-bold">{statistic.label}</p>
+              <p className="text-4xl font-bold font-palanquin">{statistic.value}</p>
+              <p className="font-bold leading-1 text-slate-gray font-palanquin">{statistic.label}</p>
             </div>
           ))}
         </div>
       </div>
 
-      <div className="relative flex flex-1 justify-center items-center
-       xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-center">
+      <div className="flex relative flex-1 justify-center items-center bg-center rounded-b-xl xl:min-h-screen max-xl:py-40">
         <img 
         src={bigShoeImg} // It is going to render which one is selected
-        alt="shose"
+        alt="shoes"
         width={610} 
         height={500}
-        className="object-contain z-10 relative"
+        className="object-contain relative z-10"
          />
 
-         <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6 ">
+         <div className="flex sm:gap-6 gap-4 absolute bottom-[2%] sm:left-[10%] max-sm:px-6 ">
           {shoes.map((shoe) => (
             <div key={shoe}>
              <ShoeCard 
