5669643bb180790140294b3f270542ccb74ac4da
Vayras <89837102+Vayras@users.noreply.github.com>
2023-08-29 12:00:57 +0530

    some ui bug fixes

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -46,7 +46,7 @@ const Hero = () => {
         className="object-contain relative z-10"
          />
 
-         <div className="flex sm:gap-6 gap-4 absolute bottom-[2%] sm:left-[10%] max-sm:px-6 ">
+         <div className="flex sm:gap-6 gap-4 absolute  -bottom-[5%] sm:left-[10%] md:left-[21%] lg:left-[10%] max-sm:px-6 ">
           {shoes.map((shoe) => (
             <div key={shoe}>
              <ShoeCard 
