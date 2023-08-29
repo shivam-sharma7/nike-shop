24ba2daefcc2d38cfa63571b3c039fc1f65ba362
LakshayDhiman023 <dhimanlakshay.02@gmail.com>
2023-08-29 16:50:06 +0530

    Add description for Popular Product #4

--- src/components/PopularProductCard.jsx
+++ src/components/PopularProductCard.jsx
@@ -1,27 +1,24 @@
 import { star } from "../assets/icons"
 
  
-const PopularProductCard = ({imgURL, name, price}) => {
+const PopularProductCard = ({imgURL, name, price, description}) => {
   return (
     <div className="flex flex-1 flex-col w-full max-sm:w-full">
-      <img
-       src={imgURL}
-       alt={name}
-       className="w-[280px] h-[280px]"/>
+      <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
 
-       <div className="mt-5 px-11 flex justify-start gap-2.5">
-        <img 
-        src={star}
-        alt="starIcon"
-        width={23}
-        height={23} />
+      <div className="mt-5 px-11 flex justify-start gap-2.5">
+        <img src={star} alt="starIcon" width={23} height={23} />
         <p className="text-slate-gray">(4.5)</p>
-       </div>
-       <h3 className="text-xl font-palanquin font-bold leading-normal ">{name}</h3>
-       <h3 className="mt-2 font-semibold font-montserrat text-coral-red text-xl leading-normal">{price}</h3>
+      </div>
+      <h3 className="text-xl font-palanquin font-bold leading-normal ">
+        {name}
+      </h3>
+      <h3 className="mt-2 font-semibold font-montserrat text-coral-red text-xl leading-normal">
+        {price}
+      </h3>
+      <p className="text-slate-gray">{description} </p>
     </div>
-    
-  )
+  );
 }
 
 export default PopularProductCard
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,124 +1,154 @@
-import { facebook, instagram, shieldTick, support, truckFast, twitter } from "../assets/icons";
-import { bigShoe1, bigShoe2, bigShoe3, customer1, customer2, shoe4, shoe5, shoe6, shoe7, thumbnailShoe1, thumbnailShoe2, thumbnailShoe3 } from "../assets/images";
+import {
+  facebook,
+  instagram,
+  shieldTick,
+  support,
+  truckFast,
+  twitter,
+} from "../assets/icons";
+import {
+  bigShoe1,
+  bigShoe2,
+  bigShoe3,
+  customer1,
+  customer2,
+  shoe4,
+  shoe5,
+  shoe6,
+  shoe7,
+  thumbnailShoe1,
+  thumbnailShoe2,
+  thumbnailShoe3,
+} from "../assets/images";
 
 export const navLinks = [
-    { href: "#home", label: "Home" },
-    { href: "#about-us", label: "About Us" },
-    { href: "#products", label: "Products" },
-    { href: "#contact-us", label: "Contact Us" },
+  { href: "#home", label: "Home" },
+  { href: "#about-us", label: "About Us" },
+  { href: "#products", label: "Products" },
+  { href: "#contact-us", label: "Contact Us" },
 ];
 
 export const shoes = [
-    {
-        thumbnail: thumbnailShoe1,
-        bigShoe: bigShoe1,
-    },
-    {
-        thumbnail: thumbnailShoe2,
-        bigShoe: bigShoe2,
-    },
-    {
-        thumbnail: thumbnailShoe3,
-        bigShoe: bigShoe3,
-    },
+  {
+    thumbnail: thumbnailShoe1,
+    bigShoe: bigShoe1,
+  },
+  {
+    thumbnail: thumbnailShoe2,
+    bigShoe: bigShoe2,
+  },
+  {
+    thumbnail: thumbnailShoe3,
+    bigShoe: bigShoe3,
+  },
 ];
 
 export const statistics = [
-    { value: '2k+', label: 'Brands' },
-    { value: '600+', label: 'Shops' },
-    { value: '300k+', label: 'Customers' },
+  { value: "2k+", label: "Brands" },
+  { value: "600+", label: "Shops" },
+  { value: "300k+", label: "Customers" },
 ];
 
 export const products = [
-    {
-        imgURL: shoe4,
-        name: "Nike Air Jordan-01",
-        price: "$300.20",
-    },
-    {
-        imgURL: shoe5,
-        name: "Nike Air Jordan-10",
-        price: "$310.30",
-    },
-    {
-        imgURL: shoe6,
-        name: "Nike Air Jordan-100",
-        price: "$250.10",
-    },
-    {
-        imgURL: shoe7,
-        name: "Nike Air Jordan-001",
-        price: "$230.20",
-    },
+  {
+    imgURL: shoe4,
+    name: "Nike Air Jordan-01",
+    price: "$300.20",
+    description:
+      "The Air Jordan-01 remakes the classic sneaker with new colors and textures. Premium materials and accents give fresh expression to an all-time favorite.",
+  },
+  {
+    imgURL: shoe5,
+    name: "Nike Air Jordan-10",
+    price: "$310.30",
+    description:
+      "Make new moves in the Air Jordan 1 Low OG. Remade with the classic details, it combines a comfortable feel with the timeless style of one of culture's greatest sneaker creations.",
+  },
+  {
+    imgURL: shoe6,
+    name: "Nike Air Jordan-100",
+    price: "$250.10",
+    description:
+      "Sophisticated and bold, this luxe take on the AJ1 delivers high-fashion bravado with equine flair. Premium leather pairs with Palomino panache, making your next 'fit smooth like MJ's game. And that pop of color on the tongue? Straight up gold.",
+  },
+  {
+    imgURL: shoe7,
+    name: "Nike Air Jordan-001",
+    price: "$230.20",
+    description:
+      "Soft suede and Jordan Brand's signature Formula 23 foam come together to give you an extra luxurious (and extra cozy) AJ1. You don't need to play 'either or' when it comes to choosing style or comfort with this one—which is nice, 'cause you deserve both.",
+  },
 ];
 
 export const services = [
-    {
-        imgURL: truckFast,
-        label: "Free shipping",
-        subtext: "Enjoy seamless shopping with our complimentary shipping service."
-    },
-    {
-        imgURL: shieldTick,
-        label: "Secure Payment",
-        subtext: "Experience worry-free transactions with our secure payment options."
-    },
-    {
-        imgURL: support,
-        label: "Love to help you",
-        subtext: "Our dedicated team is here to assist you every step of the way."
-    },
+  {
+    imgURL: truckFast,
+    label: "Free shipping",
+    subtext: "Enjoy seamless shopping with our complimentary shipping service.",
+  },
+  {
+    imgURL: shieldTick,
+    label: "Secure Payment",
+    subtext:
+      "Experience worry-free transactions with our secure payment options.",
+  },
+  {
+    imgURL: support,
+    label: "Love to help you",
+    subtext: "Our dedicated team is here to assist you every step of the way.",
+  },
 ];
 
 export const reviews = [
-    {
-        imgURL: customer1,
-        customerName: 'Morich Brown',
-        rating: 4.5,
-        feedback: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
-    },
-    {
-        imgURL: customer2,
-        customerName: 'Charlie Paul',
-        rating: 4.5,
-        feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
-    }
+  {
+    imgURL: customer1,
+    customerName: "Morich Brown",
+    rating: 4.5,
+    feedback:
+      "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!",
+  },
+  {
+    imgURL: customer2,
+    customerName: "Charlie Paul",
+    rating: 4.5,
+    feedback:
+      "The product not only met but exceeded my expectations. I'll definitely be a returning customer!",
+  },
 ];
 
-
 export const footerLinks = [
-    {
-        title: "Products",
-        links: [
-            { name: "Air Force 1", link: "/" },
-            { name: "Air Max 1", link: "/" },
-            { name: "Air Jordan 1", link: "/" },
-            { name: "Air Force 2", link: "/" },
-            { name: "Nike Waffle Racer", link: "/" },
-            { name: "Nike Cortez", link: "/" },
-        ],
-    },
-    {
-        title: "Help",
-        links: [
-            { name: "About us", link: "/" },
-            { name: "FAQs", link: "/" },
-            { name: "How it works", link: "/" },
-            { name: "Privacy policy", link: "/" },
-            { name: "Payment policy", link: "/" },
-        ],
-    },
-    {
-        title: "Get in touch",
-        links: [
-            { name: "customer@nike.com", link: "mailto:customer@nike.com" },
-            { name: "+92554862354", link: "tel:+92554862354" },
-        ],
-    },
+  {
+    title: "Products",
+    links: [
+      { name: "Air Force 1", link: "/" },
+      { name: "Air Max 1", link: "/" },
+      { name: "Air Jordan 1", link: "/" },
+      { name: "Air Force 2", link: "/" },
+      { name: "Nike Waffle Racer", link: "/" },
+      { name: "Nike Cortez", link: "/" },
+    ],
+  },
+  {
+    title: "Help",
+    links: [
+      { name: "About us", link: "/" },
+      { name: "FAQs", link: "/" },
+      { name: "How it works", link: "/" },
+      { name: "Privacy policy", link: "/" },
+      { name: "Payment policy", link: "/" },
+    ],
+  },
+  {
+    title: "Get in touch",
+    links: [
+      { name: "customer@nike.com", link: "mailto:customer@nike.com" },
+      { name: "+92554862354", link: "tel:+92554862354" },
+    ],
+  },
 ];
 
 export const socialMedia = [
-    { src: facebook, alt: "facebook logo" },
-    { src: twitter, alt: "twitter logo" },
-    { src: instagram, alt: "instagram logo" },
+  { src: facebook, alt: "facebook logo" },
+  { src: twitter, alt: "twitter logo" },
+  { src: instagram, alt: "instagram logo" },
 ];
